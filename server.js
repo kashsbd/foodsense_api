67ccd131d32d2a8fd20dcdc5e34a303de2ccbb2b
kash <parakash.sbd@gmail.com>
2023-10-20 20:38:27 -0500

    feat: working on login api

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const { validateToken } = require("./middleware");
 
@@ -28,9 +30,58 @@ server.use(express.json());
 server.use(cors());
 
 // auth api
-server.post("/login", async (req, res) => {});
+server.post("/login", async (req, res) => {
+  const body = req.body;
 
-server.post("/signup", async (req, res) => {});
+  try {
+    const hashedPassword = await bcrypt.hash(body?.password, 10);
+    const owner = await db
+      .collection("owners")
+      .findOne({ email: body?.email, password: hashedPassword });
+
+    if (owner) {
+      const token = await jwt.sign(
+        { userId: owner?._id, email: owner?.email },
+        process.env.JWT_SECRET
+      );
+    } else {
+      res.status(401).send({
+        success: false,
+        error: "Wrong email or password is provided.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ success: false, error: error?.message });
+  }
+});
+
+server.post("/signup", async (req, res) => {
+  const body = req.body;
+
+  try {
+    const allOwners = await db
+      .collection("owners")
+      .find({ email: body?.email })
+      .toArray();
+
+    if (allOwners?.length > 0) {
+      res
+        .status(409)
+        .send({ success: false, error: "Please use another email." });
+    }
+
+    const hashedPassword = await bcrypt.hash(body?.password, 10);
+    await db
+      .collection("owners")
+      .insertOne({ ...body, password: hashedPassword });
+
+    res.status(201).send({ success: true });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ success: false, error: error?.message });
+  }
+});
 
 server.use(validateToken);
 
